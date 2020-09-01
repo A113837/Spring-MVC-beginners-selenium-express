f10d827e9ef37ebb5de8cc447551e661e4071db8
A113837 <padi196@live.com>
2020-09-01 14:31:27 +0800

    Add files via upload

---
+++ 09 02 Model View Controller: Spring MVC - Full-stack tech walkthrough || Model || Data Flow-basic data at controller/src/com/home/mycafe/controllers/MyCafeControllers.java
@@ -0,0 +1,20 @@
+package com.home.mycafe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyCafeControllers {
+	
+	@RequestMapping("/cafe")
+	public String showWelcomePage(Model model) 
+	{
+		//sending data to view (jsp page)
+		String myName = " John Abhilash";
+		
+		model.addAttribute("myNameValue", myName);
+		return "welcom-page";
+	}
+
+}
